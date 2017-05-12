6879b0c809a206a21e87d860dc594535f1204263
Karim Slimani <karim@playmedia.fr>
2017-05-12 15:36:42 +0200

    chore(build): update webpack and add webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,67 @@
-var path = require('path');
-var webpack = require('webpack');
-var Clean = require('clean-webpack-plugin');
+const path = require('path')
+const webpack = require('webpack')
+const NotifierPlugin = require('webpack-notifier')
 
-var plugins = []
-var babelCompact = false
+var outputFile, plugins = []
 
-if (process.env.npm_lifecycle_event === 'dist-min') {
-  // *** Uncomment the following lines to suppress UglifyJS warnings ***
-  // plugins.push(new webpack.optimize.UglifyJsPlugin({
-  //   compress: {warnings: false},
-  //   output: {comments: false}
-  // }))
-  babelCompact = true
+if (process.env.npm_lifecycle_event === 'dist') {
+  outputFile = 'clappr-estat-plugin.min.js'
+  plugins.push(new webpack.optimize.UglifyJsPlugin({
+    output: {
+      comments: false,
+    },
+  }))
 } else {
-  plugins.push(new Clean(['dist'], {verbose: false}))
+  outputFile = 'clappr-estat-plugin.js'
 }
+
+plugins.push(new NotifierPlugin({
+  title: outputFile,
+  alwaysNotify: true,
+  // contentImage: path.resolve(__dirname, 'path/to/image.png')
+}))
 
 module.exports = {
   entry: path.resolve(__dirname, 'src/index.js'),
-  plugins: plugins,
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: outputFile,
+    library: 'ClapprEstatPlugin',
+    libraryTarget: 'umd',
+  },
   externals: {
    clappr: {
     amd: 'clappr',
     commonjs: 'clappr',
     commonjs2: 'clappr',
-    root: 'Clappr'
-   }
+    root: 'Clappr',
+   },
   },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js$/,
-        loader: 'babel',
-        exclude: /node_modules/,
-        query: {
-            compact: babelCompact,
-        }
+        loader: 'babel-loader',
+        include: [
+          path.resolve(__dirname, 'src')
+        ],
+        options: {
+          presets: ['es2015'],
+          plugins: ['add-module-exports'],
+        },
       },
     ],
   },
-  resolve: {
-    extensions: ['', '.js'],
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'clappr-estat-plugin.js',
-    library: 'ClapprEstatPlugin',
-    libraryTarget: 'umd',
-  },
-};
+  plugins: plugins,
+  devServer: {
+    contentBase: [
+      path.resolve(__dirname, "public"),
+      path.resolve(__dirname, "node_modules/clappr/dist"),
+    ],
+    // publicPath: '/js/',
+    disableHostCheck: true, // https://github.com/webpack/webpack-dev-server/issues/882
+    compress: true,
+    host: "0.0.0.0",
+    port: 8080
+  }
+}
