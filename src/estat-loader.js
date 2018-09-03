7583a8336d61560dfab912ddbf3fa4cb63f8dfb5
Karim Slimani <karim@playmedia.fr>
2018-09-03 12:02:41 +0200

    feat(estat-loader): change default version to 5.4
    Remove debug and secure optional arguments
    Force HTTPS protocol

--- src/estat-loader.js
+++ src/estat-loader.js
@@ -2,11 +2,9 @@
  * eStat "mu" library lazy loader.
  * @function
  * @param {function} The library loaded callback.
- * @param {string} The library version. (Default is '5.2')
- * @param {boolean} Set to true to load the debug/integration version of library.
- * @param {boolean} Set to true to force HTTPS load protocol. (Default behaviour is to match current protocol)
+ * @param {string} The library version. (Default is '5.4')
  */
-export default function (cb, version = '5.2', debug, secure) {
+export default function (cb, version = '5.4') {
   let win = window
   let doc = document
   let el = 'script'
@@ -17,12 +15,10 @@ export default function (cb, version = '5.2', debug, secure) {
     return
   }
 
-  let s = secure === true ? 'https:' : ''
-  let d = debug === true ? 'integration-' : ''
   let first = doc.getElementsByTagName(el)[0]
   let script = doc.createElement(el)
 
-  script.src = s + '//prof.estat.com/js/mu-' + d + version + '.js'
+  script.src = 'https://prof.estat.com/js/mu-' + version + '.js'
   script.async = true
   if (typeof cb === 'function') script.onload = cb
   first.parentNode.insertBefore(script, first)
